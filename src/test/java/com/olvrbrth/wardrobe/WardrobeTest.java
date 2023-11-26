766161c5df7d6d7c5b3c81a46f2cf494c5f73423
Oliver Barth <o.barth19@gmail.com>
2023-11-26 09:50:18 +0100

    Initial commit

---
+++ src/test/java/com/olvrbrth/wardrobe/WardrobeTest.java
@@ -0,0 +1,12 @@
+package com.olvrbrth.wardrobe;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class WardrobeTest {
+
+    @Test
+    public void simpleTest() {
+        Assert.assertTrue(true);
+    }
+}
